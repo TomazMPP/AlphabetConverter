802405158a7206f678f6b920df1e1b186a9c5d87
Tomaz <tomazmppontes@gmail.com>
2023-06-11 20:51:56 -0300

    Update

--- js/index.js
+++ js/index.js
@@ -1,92 +1,132 @@
 const basicAlphabetMapping = {
-    'A': 'А',
-    'B': 'Б',
-    'C': 'Ц',
-    'D': 'Д',
-    'E': 'Е',
-    'F': 'Ф',
-    'G': 'Г',
-    'H': 'Х',
-    'I': 'И',
-    'J': 'Й',
-    'K': 'К',
-    'L': 'Л',
-    'M': 'М',
-    'N': 'Н',
-    'O': 'О',
-    'P': 'П',
-    'Q': 'Щ',
-    'R': 'Р',
-    'S': 'С',
-    'T': 'Т',
-    'U': 'У',
-    'V': 'В',
-    'W': 'Ш',
-    'X': 'КС',
-    'Y': 'Ы',
-    'Z': 'З',
-    'a': 'а',
-    'b': 'б',
-    'c': 'ц',
-    'd': 'д',
-    'e': 'е',
-    'f': 'ф',
-    'g': 'г',
-    'h': 'х',
-    'i': 'и',
-    'j': 'й',
-    'k': 'к',
-    'l': 'л',
-    'm': 'м',
-    'n': 'н',
-    'o': 'о',
-    'p': 'п',
-    'q': 'щ',
-    'r': 'р',
-    's': 'с',
-    't': 'т',
-    'u': 'у',
-    'v': 'в',
-    'w': 'ш',
-    'x': 'кс',
-    'y': 'ы',
-    'z': 'з'
-  };
-  
-  const convertCyrillicToLatin = (text) => {
-    let convertedText = '';
-    for (let i = 0; i < text.length; i++) {
-      const char = text[i];
-      const convertedChar = basicAlphabetMapping[char] || char;
-      convertedText += convertedChar;
+  'А': 'A',
+  'Б': 'B',
+  'Ц': 'C',
+  'Д': 'D',
+  'Е': 'E',
+  'Ф': 'F',
+  'Г': 'G',
+  'Х': 'H',
+  'И': 'I',
+  'Й': 'J',
+  'К': 'K',
+  'Л': 'L',
+  'М': 'M',
+  'Н': 'N',
+  'О': 'O',
+  'П': 'P',
+  'Щ': 'Q',
+  'Р': 'R',
+  'С': 'S',
+  'Т': 'T',
+  'У': 'U',
+  'В': 'V',
+  'Ш': 'W',
+  'КС': 'X',
+  'Ы': 'Y',
+  'З': 'Z',
+  'а': 'a',
+  'б': 'b',
+  'ц': 'c',
+  'д': 'd',
+  'е': 'e',
+  'ф': 'f',
+  'г': 'g',
+  'х': 'h',
+  'и': 'i',
+  'й': 'j',
+  'к': 'k',
+  'л': 'l',
+  'м': 'm',
+  'н': 'n',
+  'о': 'o',
+  'п': 'p',
+  'щ': 'q',
+  'р': 'r',
+  'с': 's',
+  'т': 't',
+  'у': 'u',
+  'в': 'v',
+  'ш': 'w',
+  'кс': 'x',
+  'ы': 'y',
+  'з': 'z'
+};
+
+const convertCyrillicToLatin = (text) => {
+  let convertedText = '';
+  let currentChar = '';
+  for (let i = 0; i < text.length; i++) {
+    const char = text[i];
+    currentChar += char;
+    if (basicAlphabetMapping[currentChar]) {
+      convertedText += basicAlphabetMapping[currentChar];
+      currentChar = '';
     }
-    return convertedText;
-  };
-  
-  const convertLatinToCyrillic = (text) => {
-    let convertedText = '';
-    for (let i = 0; i < text.length; i++) {
-      const char = text[i];
-      const convertedChar = basicAlphabetMapping[char] || char;
-      convertedText += convertedChar;
-    }
-    return convertedText;
-  };
-  
-  const cyrillicTextarea = document.getElementById('cyrillic');
-  const latinTextarea = document.getElementById('latin');
-  
-  const updateLatinTextarea = () => {
-    const cyrillicText = cyrillicTextarea.value;
-    const convertedText = convertCyrillicToLatin(cyrillicText);
-    latinTextarea.value = convertedText;
-  };
-  
-  const updateCyrillicTextarea = () => {
-    const latinText = latinTextarea.value;
-    const convertedText = convertLatinToCyrillic(latinText);
-    cyrillicTextarea.value = convertedText;
-  };
-  
-  cyrillicTextarea.addEventListener('input', updateLatinTextarea);
-  latinTextarea.addEventListener('input', updateCyrillicTextarea);
+  }
+  if (currentChar) {
+    convertedText += currentChar;
+  }
+  return convertedText;
+};
+
+const convertLatinToCyrillic = (text) => {
+  let convertedText = '';
+  for (let i = 0; i < text.length; i++) {
+    const char = text[i];
+    const convertedChar = Object.keys(basicAlphabetMapping).find(
+      key => basicAlphabetMapping[key] === char
+    ) || char;
+    convertedText += convertedChar;
+  }
+  return convertedText;
+};
+
+const cyrillicTextarea = document.getElementById('cyrillic');
+const latinTextarea = document.getElementById('latin');
+const copyCyrillicButton = document.getElementById('copyCyrillic');
+const clearCyrillicButton = document.getElementById('clearCyrillic');
+const copyLatinButton = document.getElementById('copyLatin');
+const clearLatinButton = document.getElementById('clearLatin');
+
+const updateLatinTextarea = () => {
+  const cyrillicText = cyrillicTextarea.value;
+  const convertedText = convertCyrillicToLatin(cyrillicText);
+  latinTextarea.value = convertedText;
+};
+
+const updateCyrillicTextarea = () => {
+  const latinText = latinTextarea.value;
+  const convertedText = convertLatinToCyrillic(latinText);
+  cyrillicTextarea.value = convertedText;
+};
+
+const copyTextToClipboard = (textAreaId) => {
+  const textArea = document.getElementById(textAreaId);
+  textArea.select();
+  textArea.setSelectionRange(0, 99999);
+  document.execCommand('copy');
+};
+
+const clearTextarea = (textAreaId) => {
+  document.getElementById(textAreaId).value = '';
+};
+
+cyrillicTextarea.addEventListener('input', updateLatinTextarea);
+latinTextarea.addEventListener('input', updateCyrillicTextarea);
+
+copyCyrillicButton.addEventListener('click', () => {
+  copyTextToClipboard('cyrillic');
+});
+
+clearCyrillicButton.addEventListener('click', () => {
+  clearTextarea('cyrillic');
+});
+
+copyLatinButton.addEventListener('click', () => {
+  copyTextToClipboard('latin');
+});
+
+clearLatinButton.addEventListener('click', () => {
+  clearTextarea('latin');
+});
